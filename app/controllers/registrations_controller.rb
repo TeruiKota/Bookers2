4e28888c15549e6155bfe98789b2af7512b72c61
Terui Kota <tomako.47@gmail.com>
2019-05-25 05:04:02 +0000

    Add books views and action

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,5 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   def after_sign_up_path_for(resource)
-    about_path
+    books_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root "home#top"
   get "about" => "home#about"
+
+  resources :books
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
