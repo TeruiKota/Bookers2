7400cc00e019ef5ed31537e5eaf3323dd4bcc91c
Terui Kota <tomako.47@gmail.com>
2019-05-25 05:32:09 +0000

    add user show edit index

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get "about" => "home#about"
 
   resources :books
+  resources :users, only: [:show, :index, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
